a2b1f6068886a5960906a29590247bca2d56367d
Willicious-k <will@hpcnt.com>
2018-07-31 17:08:37 +0900

    implemented ImageModel

--- Hourglass/ImageModel.swift
+++ Hourglass/ImageModel.swift
@@ -7,3 +7,34 @@
 //
 
 import Foundation
+import Runes
+import Curry
+import Argo
+
+
+//MARK:- DataModel Description
+struct ImageList {
+  let items: [ImageItem]
+}
+
+extension ImageList: Argo.Decodable {
+  static func decode(_ json: JSON) -> Decoded<ImageList> {
+    return curry(ImageList.init)
+      <^> json <|| "items"
+  }
+}
+
+struct ImageItem {
+  let title: String
+  let link: String
+  let author: String
+}
+
+extension ImageItem: Argo.Decodable {
+  static func decode(_ json: JSON) -> Decoded<ImageItem> {
+    return curry(ImageItem.init)
+      <^> json <| "title"
+      <*> json <| "link"
+      <*> json <| "author"
+  }
+}
